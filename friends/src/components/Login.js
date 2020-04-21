b1731041fabc6e424f212e09202196e8013a1d31
tippitytapp <marc.tapp@gmail.com>
2020-04-21 15:59:53 -0400

    got the login post success

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import axios from 'axios';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 import {Route, Link} from 'react-router-dom';
 
 function Login(){
@@ -7,13 +7,25 @@ function Login(){
     const [credentials, setCredentials] = useState({username: "", password: ""})
     
     function handleChange (e){
-        setCredentials({[e.target.name]: e.target.value})
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
+    function login(e){
+        e.preventDefault();
+        axiosWithAuth()
+        .post('/api/login', credentials)
+        .then(res=>{
+            console.log(res.data.payload);
+            localStorage.setItem('token', JSON.stringify(res.data.payload));
+        })
+        .catch(err=>{
+            console.log({err})
+        })
     }
     return(
         <div className="loginForm">
         <img src="https://fontmeme.com/permalink/200421/f11e916d60807c2c2f088aea286093ce.png" alt="friends-tv-series-font" border="0"/>
             <h2>Login to See Friends</h2>
-            <form className="login" onSubmit="">
+            <form className="login" onSubmit={login}>
                 <label htmlFor="username">Username:</label>
                 <input 
                     type="text"
@@ -32,7 +44,7 @@ function Login(){
                     value={credentials.password}
                     onChange={handleChange}
                 />
-                <button className="submit">Login</button>
+                <Link to="/friends"><button className="submit">Login</button></Link>
             </form>
         </div>
     )
