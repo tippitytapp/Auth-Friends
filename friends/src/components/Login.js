49cb956684c260f9f3609e2ba1f4d037193211d0
tippitytapp <marc.tapp@gmail.com>
2020-04-21 15:29:05 -0400

    working on getting the values and onchanges to work

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 import {Route, Link} from 'react-router-dom';
 
 function Login(){
+
+    const [credentials, setCredentials] = useState({username: "", password: ""})
+    
+    function handleChange (e){
+        setCredentials({[e.target.name]: e.target.value})
+    }
     return(
         <div className="loginForm">
         <img src="https://fontmeme.com/permalink/200421/f11e916d60807c2c2f088aea286093ce.png" alt="friends-tv-series-font" border="0"/>
@@ -15,7 +21,7 @@ function Login(){
                     name="username"
                     placeholder="username"
                     value=""
-                    onChange=""
+                    onChange={handleChange}
                 />
                 <label htmlFor="password">Password:</label>
                 <input 
@@ -24,7 +30,7 @@ function Login(){
                     name="password"
                     placeholder="password"
                     value=""
-                    onChange=""
+                    onChange={handleChange}
                 />
                 <button className="submit">Login</button>
             </form>
